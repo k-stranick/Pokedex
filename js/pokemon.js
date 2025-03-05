bad9308dee20c862e934b83777bc381c691f131d
Kyle <kstranic@dtcc.edu>
2025-03-05 00:59:54 -0500

    able to pull pokemon name and images from API and display them on the page
    Can search for pokemon by name or number and display
    
    displays message when pokemon is not found

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,9 +1,9 @@
-const MAX_POKEMON = 151; // Number of Pokemon in the first generation (increase after working with the first generation)
+const MAX_POKEMON = 1051; // Number of Pokemon in the first generation (increase after working with the first generation)
 const listWrapper = document.querySelector('.list-wrapper');
 const searchInput = document.querySelector('#search-input');
-const numberFilter = document.querySelector('#number-filter');
-const nameFilter = document.querySelector('#name-filter');
-const notFoundMessage = document.querySelector('.not-found-message');
+const numberFilter = document.querySelector('#number');
+const nameFilter = document.querySelector('#name');
+const notFoundMessage = document.querySelector('#not-found-message');
 let allPokemon = []; // Array to store all Pokemon
 
 
@@ -39,16 +39,69 @@ async function fetchPokemonDataBeforeRedirect(id) {
     }
 }
 
-/**
- * 
- * @param {*} pokemonList 
- */
-async function displayPokemon(pokemonList) {
+// /**
+//  * 
+//  * @param {*} pokemonList 
+//  */
+// async function displayPokemon(pokemonList) {
+//     listWrapper.innerHTML = '';
+//     pokemonList.forEach((pokemon, index) => {
+//         const listItem = document.createElement('li');
+//         listItem.textContent = pokemon.name;
+//         listItem.dataset.index = index;
+//         listWrapper.appendChild(listItem);
+//     });
+// }
+
+async function displayPokemon(pokemon) {
     listWrapper.innerHTML = '';
-    pokemonList.forEach((pokemon, index) => {
-        const listItem = document.createElement('li');
-        listItem.textContent = pokemon.name;
-        listItem.dataset.index = index;
+
+    pokemon.forEach(pokemon => {
+        const pokemonId = pokemon.url.split('/')[6];
+        const listItem = document.createElement('div');
+        listItem.className = 'list-item';
+        listItem.innerHTML = `
+        <div class='number-wrap'>
+            <p class="caption-fonts">#${pokemonId}</p>
+        </div>      
+        <div class='img-wrap'>
+            <img src="https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/official-artwork/${pokemonId}.png" alt="${pokemon.name} />
+        </div>
+        <div class='name-wrap'>
+            <p class="body3-fonts">#${pokemon.name}</p>
+        </div> 
+        `;
+
+        listItem.addEventListener('click', async () => {
+            const isFetched = await fetchPokemonDataBeforeRedirect(pokemonId);
+            if (isFetched) {
+                window.location.href = `./pokemon-detail.html?pokemonId=${pokemonId}`;
+            }
+        });
         listWrapper.appendChild(listItem);
     });
+}
+
+
+searchInput.addEventListener("keyup", handleSearch);
+
+
+function handleSearch() {
+    const searchValue = searchInput.value.toLowerCase().trim();
+    let filteredPokemon;
+
+    if (numberFilter.checked) {
+        filteredPokemon = allPokemon.filter(pokemon => {
+            const pokemonId = pokemon.url.split('/')[6];
+            return pokemonId.startsWith(searchValue);
+        });
+    } else if (nameFilter.checked) {
+        filteredPokemon = allPokemon.filter(pokemon => pokemon.name.toLowerCase().startsWith(searchValue));
+    } else {
+        filteredPokemon = allPokemon;
+    }
+
+    displayPokemon(filteredPokemon);
+
+    notFoundMessage.style.display = filteredPokemon.length === 0 ? 'block' : 'none';
 }
