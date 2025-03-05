72064d5f63ac885c2d03489b8fd12ee372c0acc2
k-stranick <120347245+k-stranick@users.noreply.github.com>
2025-03-05 17:14:43 -0500

    adding background color to elements need to expand

--- js/pokemon-detail.js
+++ js/pokemon-detail.js
@@ -4,11 +4,11 @@ const POKEMON_SPECIES_API = 'https://pokeapi.co/api/v2/pokemon-species/';
 
 document.addEventListener('DOMContentLoaded', () => {
     const MAX_POKEMON = 1051;
-    const pokemonId = new URLSearchParams(window.location.search).get('pokemonId');
+    const pokemonId = new URLSearchParams(window.location.search).get('pokemonId'); // fixed was passing id when i needed the full name 
 
     const id = parseInt(pokemonId, 10);
 
-    if (id < 0 || id > MAX_POKEMON) {
+    if (id < 1 || id > MAX_POKEMON) {
         window.location.href = '/index.html';
         return; //prevent further code execution
     }
@@ -31,7 +31,7 @@ async function loadPokemonData(id) {
         }
 
         const [pokemonData, pokemonSpecies] = await Promise.all(responses.map(res => res.json()));
-        const abilityWrapper = document.querySelector(".pokemon-detail-wrap .pokemon-detail-move");
+        const abilityWrapper = document.querySelector(".pokemon-detail-wrap .pokemon-detail.move");
 
         abilityWrapper.innerHTML = '';
 
@@ -58,6 +58,7 @@ async function loadPokemonData(id) {
             });
         }
 
+        //pushes the history of changes to the url without reloading the page 
         window.history.pushState({}, "", `pages/pokemon-detail.html?pokemonId=${id}`);
         return { pokemonData, pokemonSpecies };
 
@@ -65,4 +66,64 @@ async function loadPokemonData(id) {
         console.error(`Failed to fetch Pokémon before redirect: ${error.message}`);
         return null;
     }
+}
+
+
+async function navigatePokemon(id) {
+    currentPokemonId = id;
+    await loadPokemonData(id);
+}
+
+const typeColors = {
+    normal: "#A8A878",
+    fire: "#F08030",
+    water: "#6890F0",
+    electric: "#F8D030",
+    grass: "#78C850",
+    ice: "#98D8D8",
+    fighting: "#C03028",
+    poison: "#A040A0",
+    ground: "#E0C068",
+    flying: "#A890F0",
+    psychic: "#F85888",
+    bug: "#A8B820",
+    rock: "#B8A038",
+    ghost: "#705898",
+    dragon: "#7038F8",
+    dark: "#705848",
+    steel: "#B8B8D0",
+    dark: "#EE99AC"
+}
+
+
+function setElementStyles(elements, cssProperty, value) {
+    elements.array.forEach(element => {
+        element.style[cssProperty] = value;
+    });
+}
+
+
+function rgbaFontHex(hexColor) {
+    return [
+        parseInt(hexColor.slice(1, 3), 16),
+        parseInt(hexColor.slice(3, 5), 16),
+        parseInt(hexColor.slice(5, 7), 16),
+    ].join(',');
+}
+
+
+function setTypeBackgroundColo(pokemon) {
+    const mainType = pokemon.types[0].type.name;
+    const color = typeColors[mainType];
+
+    if (!color) {
+        console.warn(`color notdefined for type: ${mainType}`);
+        return;
+    }
+
+    const detailMainElement = document.querySelector('.detail-main');
+    setElementStyles([detailMainElement], "backgroundColor", color);
+    setElementStyles([detailMainElement], "borderColor", color);
+    setElementStyles(document.querySelectorAll)
+
 }
